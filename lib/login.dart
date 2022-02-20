7a394fc36c52086cd2141c8839dd84bd5b339aa4
ethandsz <ethanzizu10@gmail.com>
2022-02-20 20:00:15 +0000

    Login page has background, image is messy

--- lib/login.dart
+++ lib/login.dart
@@ -27,23 +27,37 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: TextField(
-        controller: textController,
-        obscureText: true,
-        decoration: InputDecoration(
-          border: InputBorder.none,
-          hintText: "Enter 10Digit Key",
-          hintStyle: TextStyle(color: Colors.grey),
-        ),
-        onEditingComplete: () {
-          variables.tenDigitKey = textController.text;
-          print(variables.tenDigitKey);
-          if (variables.tenDigitKey.length > 9) {
-            variables.mapAccess = true;
-          }
-        },
-      )),
+      body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/loginlogo.png"), fit: BoxFit.cover),
+          ),
+          height: double.infinity,
+          width: double.infinity,
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            SizedBox(
+                height: 100,
+                width: 380,
+                child: TextField(
+                  controller: textController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    icon: Icon(Icons.send),
+                    border: OutlineInputBorder(),
+                    hintText: "Enter 10Digit Key",
+                    hintStyle: TextStyle(color: Colors.black),
+                  ),
+                  onEditingComplete: () {
+                    variables.tenDigitKey = textController.text;
+                    print(variables.tenDigitKey);
+                    if (variables.tenDigitKey.length > 9) {
+                      variables.mapAccess = true;
+                    }
+                  },
+                ))
+          ])),
     );
   }
 }
